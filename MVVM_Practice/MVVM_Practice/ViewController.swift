04b7c4926a5e12381639f7d79bcd9dd544894f87
YounByoungil <jimmy7525@naver.com>
2021-06-29 00:21:58 +0900

    Setup Project

---
+++ MVVM_Practice/MVVM_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Practice
+//
+//  Created by 윤병일 on 2021/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
